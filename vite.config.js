51e24e2e208d2eacd2b40a74abfe4ef133160aa3
piseth <pisethlongkoeun@gmail.com>
2025-01-05 23:27:11 +0700

    '2'

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server:{
-    host: true,
-    port: 80,
+  server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: 80,        // Set port to 80
   },
-})
+});
