e94437c21c5b0b51ab9db57497b145a8e242ca50
piseth <pisethlongkoeun@gmail.com>
2025-01-05 23:35:14 +0700

    '2'

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0', // Bind to all network interfaces
-    port: 80,        // Set port to 80
-  },
-});
+  plugins: [react()],
+})
